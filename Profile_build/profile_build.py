fe03d65640b1be750be0be4b6030559e55f00cda
yebof <fengyebo@gmail.com>
2022-07-05 20:48:51 -0700

    updates

--- Profile_build/profile_build.py
+++ Profile_build/profile_build.py
@@ -1,6 +1,22 @@
 # Yebo Feng
 
 import subprocess
+import read_network as rn
+import SubnetTree
+
+def if_monitor(ip1,ip2):
+    # return 1 if the first ip is in the prefixes but the second is not
+    # return 2 if the second ip is in the prefixes but the first is not
+
+    global nw_tree
+    if ip1 in nw_tree:
+        if ip2 not in nw_tree:
+            return 1
+    else:
+        if ip2 in nw_tree:
+            return 2
+    return 0
+
 
 def run_bash(command,opt):
     if opt == 1:
