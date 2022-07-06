52fab55345342a8430efe30aebae6790ca736ad7
yebof <fengyebo@gmail.com>
2022-07-05 20:12:13 -0700

    add more test cases

--- Profile_build/read_network.py
+++ Profile_build/read_network.py
@@ -22,10 +22,14 @@ def read_build_tree():
     return build_tree(read())
 
 if __name__ == "__main__":
+    # test whether the prefixes are read correctly
     t = build_tree(read())
     # test cases
     print(b'192.0.2.10' in t) # False
     print(b'142.48.190.1' in t) # True
-    print(b'192.0.2.9' in t)
-    print(b'192.0.3.9' in t)
-    print(b'1.0.0.2' in t)
+    print(b'192.0.2.9' in t) # False
+    print(b'14.181.124.2' in t) # True
+    print(b'192.0.3.9' in t) # False
+    print(b'15.73.88.3' in t) # True
+    print(b'1.0.0.2' in t) # False
+    print(b'11.58.216.5' in t) # True
