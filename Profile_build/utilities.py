980006c474ad3d5028bea1fbdfdf779a64f40250
yebof <fengyebo@gmail.com>
2022-07-13 10:10:03 -0700

    everything from yesterday

--- Profile_build/utilities.py
+++ Profile_build/utilities.py
@@ -1,11 +1,20 @@
+from datetime import datetime
 
 def read_command(filename):
+    # read nfdump command 
     command = list()
     with open(filename, 'r') as file:
         text = file.read().strip()
         command = text.split("\n")
     return command
 
+def datetime_to_timestamp():
+    dtime = datetime(2020, 8, 16, 23, 4, int(29.056))
+    timestamp = dtime.timestamp()
+    print(timestamp)
+
 if __name__ == "__main__":
     # for test
-    print(read_command("/Users/yebof/Documents/host-profiling/Profile_build/NFDUMP_command/read_single_defined.txt"))
+    # print(read_command("/Users/yebof/Documents/host-profiling/Profile_build/NFDUMP_command/read_single_defined.txt"))
+
+    datetime_to_timestamp()
