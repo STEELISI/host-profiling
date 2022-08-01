74d6b4385cc91250f3e9d55ce6b51aef002716c7
yebof <fengyebo@gmail.com>
2022-08-01 15:06:19 -0700

    update

--- Profile_build/measure.py
+++ Profile_build/measure.py
@@ -5,6 +5,20 @@ import utilities as ut
 import profile_build as pb
 import time
 
+def if_monitor(ip1,ip2):
+    # check whether one of the two IP addresses are within the defined prefixes 
+    # return 1 if the first ip is in the prefixes but the second is not
+    # return 2 if the second ip is in the prefixes but the first is not
+
+    global nw_tree
+    if ip1 in nw_tree:
+        if ip2 not in nw_tree:
+            return 1
+    else:
+        if ip2 in nw_tree:
+            return 2
+    return 0
+
 def measure_traffic_out_service(flow_list):
 
     global service_ports_dict
@@ -40,14 +54,14 @@ def measure_traffic_out_service(flow_list):
         # 2 if the second ip is in the prefixes but the first is not
         # 0 if all the ips are in the prefixes or none of the ips are in the prefixes
         # we will skip case 0 
-        prefix_flag = pb.if_monitor(ip1, ip2)
+        prefix_flag = if_monitor(ip1, ip2)
         if prefix_flag == 0:
             continue
         else:
             total_flow_number += 1
             total_packets += pkts
             total_bytes += bytes
-            if (ip1 in service_ports_dict) or (ip2 in service_ports_dict):
+            if (ip1_port in service_ports_dict) or (ip2_port in service_ports_dict):
                 continue
             else:
                 out_flow_number += 1
