22bec6c469ea8ba56a891f294dcb4d847951f17c
root <root@steellab.isi.edu>
2022-08-08 13:27:04 -0700

    add one more print function

--- Profile_build/print_pf.py
+++ Profile_build/print_pf.py
@@ -30,6 +30,36 @@ def print_nth_profile(file,num):
     print()
     print()
 
+def print_nth_simplified_profile(file,num):
+    # print the nth element in the simplified profile dictionary
+    
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, file)
+    pf_dict = ut.dict_read_from_file(filename)
+
+    it = iter(pf_dict)
+    # Consume n elements.
+    next(islice(it, num, num), None) 
+    # Return the value at the current position.
+    # This raises StopIteration if n is beyond the limits.
+    # Use next(it, None) to suppress that exception.
+    item = next(it)
+
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " + str(item) + " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+    print("Topic of this IP: ")
+    print(pf_dict[item][0])
+    print("========== " + "Traffic volume" " ===========================================================")
+    print("Outbound total pkts: " + str(pf_dict[item][1][0]))
+    print("Outbound total bytes: " + str(pf_dict[item][1][1]))
+    print("Inbound total pkts: " + str(pf_dict[item][1][2]))
+    print("Inbound total bytes: " + str(pf_dict[item][1][3]))
+    print("========== " + "Detailed port information" " ================================================")
+    for i, (k, v) in enumerate(pf_dict[item][2].items()):
+        print(k + ": " + str(v))
+    print("========== " + "End" " ======================================================================")
+    print()
+    print()
+
 def print_n_profiles(file,num):
     # print the first n elements in the profile dictionary
 
@@ -95,8 +125,11 @@ if __name__ == "__main__":
                     python3 print_pf.py -pf "results/8.17_profile_results.txt" -n 20
                 Print the 20th profile from "results/8.17_profile_results.txt":
                     python3 print_pf.py -nth_pf "results/8.17_profile_results.txt" -nth 20
+                Print the 20th simplified profile from "results/8.17_simplified_profile_results.txt":
+                    python3 print_pf.py -nth_spf "results/8.17_simplified_profile_results.txt" -nth 20
         '''))
     parser.add_argument('-spf', type=str, help='The path of the simplified profile you want to print. For example: \"results/8.17_simplified_profile_results.txt\".')
+    parser.add_argument('-nth_spf', type=str, help='The path of the simplified profile you want to print(the nth item). For example: \"results/8.17_simplified_profile_results.txt\".')
     parser.add_argument('-pf', type=str, help='The path of the profile you want to print. For example: \"results/8.17_profile_results.txt\".')
     parser.add_argument('-nth_pf', type=str, help='The path of the profile you want to print (the nth item). For example: \"results/8.17_profile_results.txt\".')
     parser.add_argument('-n', type=int, help='The number of items you want to print. For example: 20.')
@@ -109,3 +142,5 @@ if __name__ == "__main__":
         print_n_simplified_profiles(args.spf, args.n)
     elif args.nth_pf:
         print_nth_profile(args.nth_pf, args.nth)
+    elif args.nth_spf:
+        print_nth_simplified_profile(args.nth_spf, args.nth)
