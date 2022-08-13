5ad93abee69694126906089cb410eb6f1cb57fa4
yebof <fengyebo@gmail.com>
2022-08-13 16:01:36 -0700

    tiny changes

--- Profile_build/visualization.py
+++ Profile_build/visualization.py
@@ -7,13 +7,28 @@ import profile_analysis as pf_a
 
 def paint(out_service_matrix_index, normalized_out_service_matrix, out_noservice_matrix_index, normalized_out_noservice_matrix, in_service_matrix_index, normalized_in_service_matrix, in_noservice_matrix_index, normalized_in_noservice_matrix):
 
-    x_out_service = np.arange(-0.5, 288, 1)  # len = 11
-    in_noservice_port_num = len(normalized_in_noservice_matrix)
-    print(in_noservice_port_num)
-    y_out_service = np.arange(-0.5, in_noservice_port_num, 1)
-    Z_out_service = np.array(normalized_in_noservice_matrix)
     fig, ax = plt.subplots()
+    
+    # initialize x 
+    # 24*12 = 288
+    x_out_service = np.arange(-0.5, 288, 1)
+    x_out_noservice = np.arange(-0.5, 288, 1)
+    x_in_service = np.arange(-0.5, 288, 1)
+    x_in_noservice = np.arange(-0.5, 288, 1)
+
+    out_service_port_num = len(normalized_out_service_matrix)
+    y_out_service = np.arange(-0.5, out_service_port_num, 1)
+    Z_out_service = np.array(normalized_out_service_matrix)
+
     ax.pcolormesh(x_out_service, y_out_service, Z_out_service, cmap='binary', vmin=0, vmax=1)
+
+    out_noservice_port_num = len(normalized_out_noservice_matrix)
+    # errors here 
+    y_out_noservice = np.arange(-1.5, out_noservice_port_num, -1)
+    Z_out_noservice = np.array(normalized_out_noservice_matrix)
+
+    ax.pcolormesh(x_out_noservice, y_out_noservice, Z_out_noservice, cmap='binary', vmin=0, vmax=1)
+
     plt.show()
 
 if __name__ == "__main__":
