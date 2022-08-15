160fa310549229d13b83c9a75906db247506c501
yebof <fengyebo@gmail.com>
2022-08-15 16:43:56 -0700

    add rules to select hosts with bidirectial traffic

--- Profile_build/clustering.py
+++ Profile_build/clustering.py
@@ -26,6 +26,33 @@ def select_n_randomly(n, from_file, to_file):
     filename_write = os.path.join(to_dirname, to_file)
     ut.dict_write_to_file(new_dict, filename_write)
 
+def select_n_bidirectional_randomly(n, from_file, to_file):
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, from_file)
+    spf_dict = ut.dict_read_from_file(filename)
+
+    spf_dict_key = list(spf_dict.keys())
+    new_dict = dict()
+
+    num = 0
+    while num <= n:
+        ip = random.choice(spf_dict_key)
+        if ip in new_dict:
+            continue
+        if spf_dict[ip][1][1] >= 100 and spf_dict[ip][1][3] >= 100:
+            num += 1
+            temp_dict = dict()
+            temp_dict = copy.deepcopy(spf_dict[ip][2])
+            new_dict[ip] = dict()
+            for i, (k, v) in enumerate(temp_dict.items()):
+                new_dict[ip][k] = v[1]
+        else:
+            continue
+    
+    to_dirname = os.path.dirname(__file__)
+    filename_write = os.path.join(to_dirname, to_file)
+    ut.dict_write_to_file(new_dict, filename_write)
+
 def clustering(file):
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, file)
@@ -45,4 +72,5 @@ def clustering(file):
 
 if __name__ == "__main__":
     # select_n_randomly(100, "results/8.17_simplified_profile_results.txt", "sampled_100_simplified_profile.txt")
+    # select_n_bidirectional_randomly(100, "results/8.17_simplified_profile_results.txt", "sampled_100_simplified_profile.txt")
     clustering("sampled_100_simplified_profile.txt")
