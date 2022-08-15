4eb31b98f6e5749a87c3becd7b0e3fcff64a2831
yebof <fengyebo@gmail.com>
2022-08-15 09:30:32 -0700

    kick off the hierarchical clustering

--- Profile_build/install.sh
+++ Profile_build/install.sh
@@ -1,7 +1,8 @@
-# usage: bash install.sh
+# pip3 install -r requirements.txt
 
 pip3 install pysubnettree
 pip3 install pymongo
 pip3 install argparse
 pip3 install pandas
 pip3 install numpy
+pip3 install seaborn
