41a76bb99c012f38f3a32973953bf086f7b3fe60
yebof <fengyebo@gmail.com>
2022-08-24 00:27:44 -0700

    working on count port usage for each host

--- Profile_build/measure.py
+++ Profile_build/measure.py
@@ -7,6 +7,7 @@ import profile_build as pb
 import time
 import os
 import argparse
+import read_network as rn
 
 def if_monitor(ip1,ip2):
     # check whether one of the two IP addresses are within the defined prefixes 
@@ -117,7 +118,94 @@ def port_usage_count(flow_list):
                 port_usage_dict[ip2_port] = [pkts, bytes]
             # print(port_usage_dict[ip2_port])
 
+def port_usage_count_for_each_IP(flow_list):
+    global service_ports_dict
+    global port_usage_dict_for_IP
+
+    for record in flow_list:
+        items = record.split("|")
+
+        # ip1 is the source ip 
+        # ip2 is the destination ip
+        ip1_and_port = items[3].strip().split(":")
+        ip2_and_port = items[4].strip().split(":")
+        ip1 = ip1_and_port[0]
+        ip1_port = ip1_and_port[1]
+        ip2 = ip2_and_port[0]
+        ip2_port = ip2_and_port[1]
+        # print(ip1, ip1_port, ip2, ip2_port)
+        pkts = int(items[7].strip())
+        bytes = int(items[8].strip())
+
+        
+
+def measure_port_usage_for_each_ip(path_of_files, save_to_file):
+    # Count the port usage for each endpoint separately. 
+
+    # read service ports 
+    global service_ports_dict
+    service_ports_dict = ut.service_port_to_dict("service-names-port-numbers.csv")
+    # initialize and read the prefix
+    global nw_tree
+    nw_tree = rn.read_build_tree()
+    print("Successfully read the prefixes!")
+
+    # initialize the dict to store the results (port usage for each endpoint)
+    global port_usage_dict_for_IP
+    port_usage_dict_for_IP = {}
+
+    files = ut.get_files(path_of_files)
+
+    # read command 
+    try:
+        dirname = os.path.dirname(__file__)
+        command_filename = os.path.join(dirname, "NFDUMP_command/read_single.txt")
+        command = ut.read_command(command_filename)
+        # command = ' '.join(command)
+        print("Successfully read the command:")
+        print("\t"+' '.join(command))
+    except Exception as e:
+        print("An exception occurred when reading the command!")
+        print(e)
+    
+    # run the command and read the Netflow data
+    measure_start_time = time.time()
+
+    try:
+        for file in files:
+            runtime_start = time.time()
+            print("Inputting the 5 mins of NetFlow data now...")
+            print(file)
+            command[2] = file
+
+            # run with opt 2 as it is complicated output 
+            NF_input = pb.run_bash(' '.join(command),2)
+            NF_input = NF_input.strip().split("\n")
+            print("NetFlow input successfully!")
+
+            # build profiles from the netflow data 
+            port_usage_count_for_each_IP(NF_input)
+            runtime_end = time.time()
+            runtime = float(runtime_end - runtime_start)
+
+            print("~"*10 + " Port usage counted for " + file + " " + "~"*10)
+            print("~"*10 + " Toke " + str(runtime) +"s " + "~"*10)
+    except Exception as e:
+        print("An exception occurred when building profiles from the NetFlow data!")
+        print(e)
+
+    # save file
+    the_dirname = os.path.dirname(__file__)
+    save_to_filename = os.path.join(the_dirname, save_to_file)
+    ut.dict_write_to_file(port_usage_dict_for_IP, save_to_filename)
+    
+    measure_end_time = time.time()
+    measure_time_taken = measure_end_time - measure_start_time
+    print("Total time taken: " + str(measure_time_taken) + "s.")
+
 def measure_port_usage(path_of_files):
+    # Count the port usage for all flows in the Netflow (not for each endpoint separately). 
+
     # read service ports 
     global service_ports_dict
     service_ports_dict = ut.service_port_to_dict("service-names-port-numbers.csv")
